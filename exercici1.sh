fd2b5f63b843e97ba6cb7a9f3785393dc3ecb9cc
Isaac Palou <isaacpalou@paucasesnovescifp.cat>
2022-02-18 16:02:56 +0100

    feat: :sparkles: Añadir primer ejercicio bash

---
+++ exercici1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo Introduce tu nombre
+read nombre
+echo Introduce tu primer apellido
+read primer
+echo Introduce tu segundo apellido
+read segundo
+echo Tu nombre es $nombre $primer $segundo
